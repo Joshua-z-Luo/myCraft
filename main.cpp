bb0625da70a383c9fcd5080934eddada983be89e
jla818 <jla818@sfu.ca>
2024-01-08 22:54:00 -0800

    created cube with texture on every face

--- main.cpp
+++ main.cpp
@@ -20,26 +20,69 @@ const unsigned int width = 800;
 const unsigned int height = 800;
 
 
+
 // Vertices coordinates
 GLfloat vertices[] =
-{ //     COORDINATES     /        COLORS      /   TexCoord  //
-	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
-	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
-	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
-	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
-	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
+{
+	// Front face
+   -0.5f, -0.5f,  0.5f,  0.83f, 0.70f, 0.44f,   0.0f, 0.0f, // 0 - Bottom-left
+	0.5f, -0.5f,  0.5f,  0.83f, 0.70f, 0.44f,   1.0f, 0.0f, // 1 - Bottom-right
+	0.5f,  0.5f,  0.5f,  0.83f, 0.70f, 0.44f,   1.0f, 1.0f, // 2 - Top-right
+   -0.5f,  0.5f,  0.5f,  0.83f, 0.70f, 0.44f,   0.0f, 1.0f, // 3 - Top-left
+
+   // Back face
+  -0.5f, -0.5f, -0.5f,  0.92f, 0.86f, 0.76f,   0.0f, 0.0f, // 4 - Bottom-left
+   0.5f, -0.5f, -0.5f,  0.92f, 0.86f, 0.76f,   1.0f, 0.0f, // 5 - Bottom-right
+   0.5f,  0.5f, -0.5f,  0.92f, 0.86f, 0.76f,   1.0f, 1.0f, // 6 - Top-right
+  -0.5f,  0.5f, -0.5f,  0.92f, 0.86f, 0.76f,   0.0f, 1.0f, // 7 - Top-left
+
+  // Right face
+  0.5f, -0.5f,  0.5f,  0.76f, 0.45f, 0.12f,   0.0f, 0.0f, // 8 - Bottom-front
+  0.5f, -0.5f, -0.5f,  0.76f, 0.45f, 0.12f,   1.0f, 0.0f, // 9 - Bottom-back
+  0.5f,  0.5f, -0.5f,  0.76f, 0.45f, 0.12f,   1.0f, 1.0f, // 10 - Top-back
+  0.5f,  0.5f,  0.5f,  0.76f, 0.45f, 0.12f,   0.0f, 1.0f, // 11 - Top-front
+
+  // Left face
+ -0.5f, -0.5f,  0.5f,  0.76f, 0.45f, 0.12f,   0.0f, 0.0f, // 12 - Bottom-front
+ -0.5f, -0.5f, -0.5f,  0.76f, 0.45f, 0.12f,   1.0f, 0.0f, // 13 - Bottom-back
+ -0.5f,  0.5f, -0.5f,  0.76f, 0.45f, 0.12f,   1.0f, 1.0f, // 14 - Top-back
+ -0.5f,  0.5f,  0.5f,  0.76f, 0.45f, 0.12f,   0.0f, 1.0f, // 15 - Top-front
+
+ // Top face
+-0.5f,  0.5f,  0.5f,  0.67f, 0.87f, 0.50f,   0.0f, 0.0f, // 16 - Front-left
+ 0.5f,  0.5f,  0.5f,  0.67f, 0.87f, 0.50f,   1.0f, 0.0f, // 17 - Front-right
+ 0.5f,  0.5f, -0.5f,  0.67f, 0.87f, 0.50f,   1.0f, 1.0f, // 18 - Back-right
+-0.5f,  0.5f, -0.5f,  0.67f, 0.87f, 0.50f,   0.0f, 1.0f, // 19 - Back-left
+
+// Bottom face
+-0.5f, -0.5f,  0.5f,  0.98f, 0.76f, 0.45f,   0.0f, 0.0f, // 20 - Front-left
+ 0.5f, -0.5f,  0.5f,  0.98f, 0.76f, 0.45f,   1.0f, 0.0f, // 21 - Front-right
+ 0.5f, -0.5f, -0.5f,  0.98f, 0.76f, 0.45f,   1.0f, 1.0f, // 22 - Back-right
+-0.5f, -0.5f, -0.5f,  0.98f, 0.76f, 0.45f,   0.0f, 1.0f  // 23 - Back-left
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
 	0, 1, 2,
-	0, 2, 3,
-	0, 1, 4,
-	1, 2, 4,
-	2, 3, 4,
-	3, 0, 4
+	2, 3, 0,
+
+	4, 5, 6,
+	6, 7, 4,
+
+	8, 9, 10,
+	10, 11, 8,
+
+	12, 13, 14,
+	14, 15, 12,
+
+	16, 17, 18,
+	18, 19, 16,
+
+	20, 21, 22,
+	22, 23, 20
 };
+
 
 
 
@@ -94,6 +137,10 @@ int main()
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
 	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
+	// Link VBO for texture coordinates
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
